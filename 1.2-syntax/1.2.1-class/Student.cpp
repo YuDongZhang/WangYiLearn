5771f3dac5967322ddfab464fb815a325c5b975e
zhangyd <zhangyd@yuntian.com>
2023-12-28 15:33:35 +0800

    先备份一下

--- 1.2-syntax/1.2.1-class/Student.cpp
+++ 1.2-syntax/1.2.1-class/Student.cpp
@@ -6,6 +6,14 @@
 
 Student* Student::instance = 0;//表示instance 属于 student
 
+Student::Student() {
+    // 构造函数的实现
+}
+
+Student::~Student() {
+    // 析构函数的实现
+}
+
 Student* Student::getInstance() {
     if (!instance) {
         instance = new Student();
--- 1.2-syntax/1.2.1-class/main.cpp
+++ 1.2-syntax/1.2.1-class/main.cpp
@@ -3,27 +3,13 @@
 // #include "Teacher.h"
 
 using namespace std;
-class Teacher
-{
-    int i;
-private:
-    int j;
 
-public:
-    Teacher(){
-
-    };
-    ~Teacher(){
-
-    };
-    
-};
 
 int main(){
     // Student* student = Student::getInstance();
     // std::cout << "hello world";
-    Teacher teacher;
-    std::cout<< "heoll world \n";
+    // Teacher teacher;
+    std::cout<< "heoll worldx 中文 \n";
  
 }
 
